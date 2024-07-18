859e24f480a97a2a5747677402b25efe9a055d5b
Michal <michalgrzegorczyk04@gmail.com>
2024-07-18 18:59:44 +0200

    add progress with side effect

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,12 +1,22 @@
 /** @format */
 
+import { useState } from "react";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingState, setRemainingState] = useState(duration * 1000);
+
+  setInterval(function () {
+    setRemainingState((prevTime) => prevTime - 50);
+  }, 50);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
+      <p>
+        <progress max={duration * 1000} value={remainingState} />
+      </p>
       <p>{duration}</p>
     </Container>
   );
